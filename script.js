0fc2c15cbab4b17e69615dd8744cfc27d63896ec
JORDAN DIAZ <65864414+Jordandiazfr@users.noreply.github.com>
2021-01-18 19:32:09 +0100

    new desing

--- script.js
+++ script.js
@@ -9,17 +9,45 @@ var savedList = localStorage.getItem("simplon");
 // Add student
 
 // bind input students
-nameStudent.addEventListener("input", (e) => {
+nameStudent.addEventListener("input", function (e) {
   nameStudent.value = e.target.value;
 });
+
+nameStudent.addEventListener("click", (e) => {
+  nameStudent.value = "";
+});
+
+function clean() {
+  simplon = [];
+  nameStudent.value = "";
+  numStudents.value = "";
+}
+// Manipulate the dom
+function createDOM(what, how, where) {
+  let myDiv = document.createElement("div");
+  let myP = document.createElement("p");
+  myP.innerText = what;
+  myDiv.setAttribute("class", how);
+  where.appendChild(myDiv);
+  myDiv.appendChild(myP);
+}
+
+// reload the DOM
+function reload(element) {
+  var container = document.querySelector(element);
+  var content = container.innerHTML;
+  container.innerHTML = content;
+}
 
 // Add the student to the selected div and  also to an array
 document.querySelector(".add").addEventListener("click", (e) => {
   e.preventDefault();
   if (nameStudent.value == "") {
-    alert("Please insert an student name or load the past list");
+    nameStudent.value = "You must add someone";
+  } else if (nameStudent.value == "You must add someone") {
+    nameStudent.value = "";
   } else {
-    studentList.insertAdjacentHTML("beforeEnd", nameStudent.value + "<br>");
+    createDOM(nameStudent.value, "student", studentList);
     simplon.push(nameStudent.value);
     localStorage.setItem("simplon", simplon);
     nameStudent.value = "";
@@ -37,50 +65,46 @@ document.querySelector(".makeGroups").addEventListener("click", (e) => {
   e.preventDefault();
   if (numStudents.value == "") {
     alert("Please specify how many students per group");
-  }
-  // ----- The hard logic comes here ----------
-  finalGroup.innerHTML = "";
-  const total = makeGroups(numStudents.value);
-  console.log(total);
+  } else {
+    // ----- The hard logic comes here ----------
+    finalGroup.innerHTML = "";
+    const total = makeGroups(numStudents.value);
+    console.log(total);
 
-  // ----- The hard logic ENDS here ----------
+    // ----- The hard logic ENDS here ----------
 
-  // Print
+    // Print
 
-  for (i = 0; i < total.length; i++) {
-    finalGroup.insertAdjacentHTML(
-      "beforeEnd",
-      "<h1>Group" + (i + 1) + "</h1>" + "<h2>" + total[i] + "</h2>"
-    );
+    for (i = 0; i < total.length; i++) {
+      let myP = document.createElement("p");
+      myP.innerText = "Group " + (i + 1);
+      myP.setAttribute("class", "grouptitle");
+      finalGroup.appendChild(myP);
+      createDOM(total[i], "student", finalGroup);
+    }
   }
 });
 
 // LOAD THE LOCAL STORAGED LIST
 btnLoad.addEventListener("click", (e) => {
   e.preventDefault();
-
-  simplon = savedList.split(",");
-  // print each different line
-  for (i = 0; i < simplon.length; i++) {
-    studentList.insertAdjacentHTML("beforeEnd", simplon[i] + "<br>");
+  console.log(simplon);
+  if (simplon.length == 0) {
+    simplon = savedList.split(",");
+    // print each different line
+    for (i = 0; i < simplon.length; i++) {
+      createDOM(simplon[i], "student", studentList);
+    }
   }
 });
 
-if (typeof Storage !== "undefined") {
-  // Code for localStorage/sessionStorage.
-  // Store value
-  // Retrieve value from local storage and assign to variable
-} else {
-  // Sorry! No Web Storage support..
-}
-
-// ------------------------- Logique -----------------------------
+// ------------------------- Logic -----------------------------
 
 // Make a list of students
 // list of final group
 // list of remaining students
 // list of current group selon le nombre
-// shuffile
+// shuffle
 //  while il y a de remaining students
 // select pop an student
 // current group has space ?  push it
@@ -88,12 +112,12 @@ if (typeof Storage !== "undefined") {
 
 const makeGroups = (maxNum) => {
   let allGroups = [];
-  var remainingStudents = [...simplon];
+  remainingStudents = [...simplon];
+  remainingStudents.sort(() => Math.random() - 0.5);
   let currentGroup = [];
-  console.log(remainingStudents);
+
   while (remainingStudents.length > 0) {
-    let r = randomInt(0, remainingStudents.length);
-    var currentStudent = remainingStudents.pop(r);
+    var currentStudent = remainingStudents.pop();
     console.log(currentStudent);
     if (currentGroup.length >= maxNum) {
       allGroups.push(currentGroup);
@@ -102,9 +126,7 @@ const makeGroups = (maxNum) => {
     currentGroup.push(currentStudent);
   }
   allGroups.push(currentGroup);
+  console.log(allGroups);
   return allGroups;
 };
-
-const randomInt = (min, max) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+//////////// Visual Effects //////////
